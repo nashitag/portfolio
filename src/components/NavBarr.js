1fc43f5417302d3dd3ba0c77c44e6df244ac6c83
Nash <nashitag@gmail.com>
2022-03-11 11:12:33 +0400

    transparency nav bar on mobile

--- src/components/NavBarr.js
+++ src/components/NavBarr.js
@@ -18,19 +18,19 @@ function Navbarr() {
 
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="/work">Work</Nav.Link>
-                <Nav.Link href="/education">Education & Certifications</Nav.Link>
-                {/* <Nav.Link href="/experience">Experiec</Nav.Link> */}
-                <Nav.Link href="/awards">Awards</Nav.Link>
-                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown> */}
-            </Nav>
+                <Nav className="me-auto" >
+                    <Nav.Link href="/work"  style={{backgroundColor:matches?'transparent':'#f4f4f4'}}>Work</Nav.Link>
+                    <Nav.Link href="/education" style={{backgroundColor:matches?'transparent':'#f4f4f4'}}>Education & Certifications</Nav.Link>
+                    {/* <Nav.Link href="/experience">Experiec</Nav.Link> */}
+                    <Nav.Link href="/awards" style={{backgroundColor:matches?'transparent':'#f4f4f4'}}>Awards</Nav.Link>
+                    {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown> */}
+                </Nav>
             </Navbar.Collapse>
         </Container>
         </Navbar>
